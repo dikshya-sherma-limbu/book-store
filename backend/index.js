17cbf5c9fda338c505d0d83bf365d6b45f21fe53
dikshya limbu <dshermal@my.centennialcollege.ca>
2025-01-24 12:36:40 -0500

    backend-PR-5/setup: Setup is done for the backend | 1 POST end poit for book as well

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+//middleware
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+//routes
+const bookRoute = require("./src/endpoints/bookRoute");
+app.use("/api/books", bookRoute);
+
+main()
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.DB_URL);
+}
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
