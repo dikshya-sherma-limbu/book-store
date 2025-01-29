2df63e426794dde6eb63cafa57b028a7ae7a08a2
dikshya limbu <dshermal@my.centennialcollege.ca>
2025-01-29 12:37:44 -0500

    added user log out functionality and user state management with firebase onAuthStateChanged

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,7 +1,9 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
 } from "firebase/auth";
 import { auth } from "../firebase/firebase.config";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
@@ -16,7 +18,7 @@ const useAuth = () => {
 const googleProvider = new GoogleAuthProvider();
 
 // AuthProvider - provides the authentication context to the app
-export const AuthPorvider = ({ children }) => {
+const AuthPorvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -35,12 +37,42 @@ export const AuthPorvider = ({ children }) => {
     return await signInWithPopup(auth, googleProvider);
   };
 
+  // log out user
+  const logoutUser = async () => {
+    return await signOut(auth);
+  };
+
+  // manage user state
+  useEffect(() => {
+    // Setting up an authentication state listener using Firebase's onAuthStateChanged
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      // Whenever the auth state changes (sign-in/sign-out), this callback runs
+      setCurrentUser(user);
+      setLoading(false);
+
+      if (user !== null) {
+        const { email, displayName, photoURL } = user;
+        const userData = {
+          email,
+          username: displayName,
+          photo: photoURL,
+        };
+      }
+    });
+
+    // Cleanup function (runs when the component unmounts or effect is cleaned up):
+    // This removes the listener set up by onAuthStateChanged when the component
+    // unmounts or before the effect runs again (if the dependency array changes).
+    return () => unsubscribe(); // Unsubscribing from the auth state change listener
+  }, []); // Empty dependency array means this runs only once on mount and unmount
+
   const value = {
     currentUser,
     registerUser,
     loginUser,
     signInWithGoogle,
+    logoutUser,
   };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
-export { AuthContext, useAuth };
+export { AuthContext, useAuth, AuthPorvider };
