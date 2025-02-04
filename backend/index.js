44fdd1425584770d1b01b044bc1835f44d003bc4
dikshya limbu <dshermal@my.centennialcollege.ca>
2025-02-04 00:16:15 -0500

    feature-PR-11: added feature to update the book

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ console.log(process.env.PORT);
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5174"],
+    origin: ["http://localhost:5173"],
     credentials: true,
   })
 );
